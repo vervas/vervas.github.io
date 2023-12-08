5d3983ed86a208435789793ca976c29c02d3a771
Dimitrios Verraros <dverraros@apple.com>
2023-12-08 14:19:47 +0100

    Reorder returned values

--- assets/greetings/sentence.py
+++ assets/greetings/sentence.py
@@ -1,10 +1,10 @@
 from secrets import choice
-
+from typing import Optional
 
 GREETINGS = [
     "Have a fantastic Monday!",
 ]
 
 
-def get_greeting() -> str:
-    return choice(GREETINGS)
+def get_greeting() -> tuple[str, Optional[str], Optional[str], Optional[str]]:
+    return choice(GREETINGS), None, None, None
