e15ca06103ae98c268130c09198ff7203c98ca2d
Dimitrios Verraros <dverraros@apple.com>
2023-12-08 14:36:19 +0100

    Export greeting as a json payload

--- assets/greetings/sentence.py
+++ assets/greetings/sentence.py
@@ -1,5 +1,7 @@
+import json
+from pathlib import Path
 from secrets import choice
-from typing import Optional
+from typing import Optional, Union
 
 GREETINGS = [
     "Have a fantastic Monday!",
@@ -8,3 +10,35 @@ GREETINGS = [
 
 def get_greeting() -> tuple[str, Optional[str], Optional[str], Optional[str]]:
     return choice(GREETINGS), None, None, None
+
+
+def make_greeting(
+    greeting: str,
+    subject: Optional[str],
+    sender: Optional[str],
+    recipients: Optional[list[str]],
+) -> dict[str, Union[str, list[str], None]]:
+    payload = {
+        "greeting": greeting,
+        "subject": subject,
+        "sender": sender,
+        "recipients": recipients,
+    }
+
+    return payload
+
+
+def main() -> None:
+    greeting = make_greeting(*get_greeting())
+
+    output_file = Path(__file__).parent.with_name("greeting.json")
+    output_file.write_text(
+        json.dumps(
+            greeting,
+            indent=4,
+        )
+    )
+
+
+if __name__ == "__main__":
+    main()
